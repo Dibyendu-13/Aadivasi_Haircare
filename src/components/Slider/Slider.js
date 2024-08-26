9f119cf9f800788b7d59614dd06dcf51d9bb2069
Dibyendu <dibyendubar13702@gmail.com>
2024-08-26 14:08:59 +0530

    prod-2

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -8,7 +8,7 @@ import './Slider.css'; // Custom styles for the slider
 import image1 from '../../assets/Hairoil-1.jpg';
 import image2 from '../../assets/Hairoil-2.jpg';
 import image3 from '../../assets/Hairoil-3.jpg';
-import video1 from '../../assets/Aadivasi_Community_Latest_Video.mp4';
+import video1 from '../../assets/Aadivasi_Community_Latest_Video - Made with Clipchamp.mp4';
 
 // Array of images and video, with video first
 const media = [
